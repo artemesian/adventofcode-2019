52f78689d10c3c591f3e2763d09baff69053a43a
angeloDiepe <angelodiepe10@yahoo.com>
2019-12-06 22:27:00 +0100

    Adventofcode Day4 Complete

--- Day4/Day4.js
+++ Day4/Day4.js
@@ -1,30 +1,41 @@
-var range = ["130254","678275"];
-var shallowRange1 = range[0].split('').map(el=>Number(el))
-var shallowRange2 = range[1].split('').map(el=>Number(el))
-console.log(shallowRange1,shallowRange2);
-const getDeep = (start,end) =>{
-	let deep = []
-	for (var i = start ; i <= end; i++) {
-		deep.push(i)
+let range = [130254,678275];
+let combination = [];
+for (let i = 130254; i <=678275; i++) {
+	combination.push(i)
+}
+//Part I
+const checkCombination = comb =>{
+	let double = false
+	for (let i = 1; i <= comb.length-1; i++) {
+		if(comb[i-1]>comb[i]) return false;
+		if(comb[i-1]===comb[i] && !double) {
+			double=true;
+		};
 	}
-	return deep
+	return double;
 }
-let deepRange = [];
-for (var i = 0; i < shallowRange1.length-1; i++) {
-	deepRange.push(getDeep(shallowRange1[i],shallowRange2[i]))
-}
-var double = false;
-var combination = [];
+var validCombination = combination.filter(el=>checkCombination(el.toString()))
 
-function goDeepDownRight (down,right) {
-	if (deepRange[down + 1] && test) {
-		goDeepDownRight(down+1,right)	
+console.log(validCombination.length)
+
+//Part II
+	const checkCombination2 = comb =>{
+	let double = false;
+	let doubleTest = false;
+	for (let i = 1; i <= comb.length; i++) {
+		if(comb[i-1]>comb[i]) return false;
+		if(comb[i-1]===comb[i]) {
+			double=true;
+			if(comb[i-2]===comb[i-1]) {
+				double=false;
+			};
+		}
+		else if(double){
+			doubleTest = true;
+		}
 	}
-	if(deepRange[down] && !test && deepRange[down] )
-		goDeepDownRight(down,right+1)
+	return doubleTest;
 }
+var validCombination2 = combination.filter(el=>checkCombination2(el.toString()))
 
-for (var i = 0; i < deepRange[0].length; i++) {
-	
-}
-console.log(deepRange);
+console.log(validCombination2.length)
